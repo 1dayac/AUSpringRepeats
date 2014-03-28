1859fdf8fe5cde3dd2eafa46467133e72519704d
1dayac <1dayac@rambler.ru>
2014-03-28 22:42:06 +0400

    New commit

--- TSDFinder.cpp
+++ TSDFinder.cpp
@@ -18,17 +18,44 @@
 
 struct Repeat
 {
-  Repeat(std::string _family, int _start, int _end)
-    : family(_family), start(_start), end(_end)
+  Repeat(std::string _family, int _start, int _end, std::string classOfRepeat, std::string chromName, char _strand)
+    : family(_family), start(_start), end(_end), name(classOfRepeat), targetChromosome(chromName), strand(_strand), isTSD(false)
   { }
   Repeat()
     : family(""), start(0), end(0)
   { }
 
-  std::string family;
-  int start;
+  std::string targetChromosome;
+
+  std::string family;  //L1HS
+  std::string name;    //LINE1
+
+  int start;                 
   int end;
+  char strand;
+  std::string leftTSD;
+  int startPosLeftTSD;
+  std::string rightTSD;
+  int startPosRightTSD;
+  int lenghtOfTSD;
+  int editDistance;
+  bool isTSD;
+  void Print(std::ofstream& out)
+  {
+
+    out << targetChromosome << ";" <<name << ";" << family << ";" << start << ";" << end << ";" << strand << ";";
+    if(isTSD)
+    {
+      out << 1 << ";" << leftTSD << ";" << startPosLeftTSD << ";" << rightTSD << ";" << startPosRightTSD << ";" << lenghtOfTSD << ";" << editDistance << std::endl;
+    }
+    else
+    {
+      out << 0 << std::endl;
+    }
+  }
 };
+
+
 
 const int MAXPENALTY = 3;
 const int INFTY = 1000;
@@ -46,7 +73,7 @@ void readLengthes(std::string filename)
   while(in >> temp)
   {
     in >>temp2 >> temp3;
-    expectedLengthOfRepeat[temp] = temp2;
+    expectedLengthOfRepeat[temp3] = std::max(temp2, expectedLengthOfRepeat[temp3]);
   }
 }
 
@@ -62,17 +89,21 @@ void fillAnnotation(std::string& filename)
   std::string familyOfRepeat = "";
   while(in >> temp)
   {
-    for(int i = 0; i < 4; ++i)
+    for(int i = 0; i < 3; ++i)
       in >> temp;
+    std::string chromName = "";
+
+    in >> chromName;
     in >> startPos;
     in >> endPos;
-    for(int i = 0; i < 2; ++i)
-      in >> temp;
+    in >> temp;
+    char strand;
+    in >> strand;
     in >> familyOfRepeat;
     std::string classOfRepeat = "";
     in >> classOfRepeat;
-    if(classOfRepeat.substr(0, 4) == "LINE" || classOfRepeat.substr(0, 4) == "SINE" || classOfRepeat.substr(0, 3) == "LTR")
-      mapOfAnnotation[startPos] = Repeat(familyOfRepeat, startPos, endPos);
+    if(classOfRepeat.substr(0, 4) == "LINE")
+      mapOfAnnotation[startPos] = Repeat(familyOfRepeat, startPos, endPos, classOfRepeat, chromName, strand);
     std::getline(in, temp);
   }
 }
@@ -87,51 +118,50 @@ void readGenome(std::string& genome, std::ifstream& in)
 }
 
 
-void alignOverlapTSD(std::string left, std::string right, std::ofstream& out, size_t startOfRepeat)
+void alignOverlapTSD(std::string left, std::string right, std::ofstream& out, size_t startOfRepeat, Repeat myRepeat)
 {
   if(std::min(left.length(), right.length()) < 6)
     return;
   std::vector<std::vector<int>> matrix(left.length() + 1, std::vector<int>(right.length() + 1));
   std::vector<std::vector<int>> path(left.length() + 1, std::vector<int>(right.length() + 1));
+  int maxScore = -INFTY;
+  int posX = 0;
+  int posY = 0;
   for(int i = 1; i <= left.length(); ++i)
   {
     for(int j = 1; j <= right.length(); ++j)
     {
       if(left[i - 1] == right[j - 1])
       {
-        matrix[i][j] = std::min(matrix[i - 1][j - 1], std::min(matrix[i - 1][j] + 1, matrix[i][j - 1] + 1));
+        matrix[i][j] = std::max(matrix[i - 1][j - 1] + 1, std::max(matrix[i - 1][j] - 3, std::max(matrix[i][j - 1] - 3, 0)));
+        if(matrix[i][j] > maxScore)
+        {
+          maxScore = matrix[i][j];
+          posX = i;
+          posY = j;
+        }
       }
       else
       {
-        matrix[i][j] = std::min(matrix[i - 1][j - 1] + 1, std::min(matrix[i - 1][j] + 1, matrix[i][j - 1] + 1));
+        matrix[i][j] = std::max(matrix[i - 1][j - 1] - 3, std::max(matrix[i - 1][j] - 3, std::max(matrix[i][j - 1] - 3, 0)));
       }
     }
   }
 
-  int position = 0;
-  int distance = INFTY;
-  for(int i = 5; i < right.length(); ++i)
-  {
-    if(matrix[left.length()][i] <= distance)
-    {
-      position = i;
-      distance = matrix[left.length()][i];
-    }
-  }
 
-  if(distance > MAXPENALTY)
-    return;
   //Backtrack
+
   std::string answer1 = "";
   std::string answer2 = "";
-  int xPos = left.length();
-  int yPos = position;
+  int xPos = posX;
+  int yPos = posY;
   int numberOfLetterOfA = 0;
   int numberOfLetterOfB = 0;
 
-  while(xPos != 0 && yPos != 0)
+
+  while(matrix[xPos][yPos] != 0)
   {
-    if(left[xPos - 1] == right[yPos - 1] && matrix[xPos][yPos] == matrix[xPos - 1][yPos - 1])
+    if(left[xPos - 1] == right[yPos - 1] && matrix[xPos][yPos] == matrix[xPos - 1][yPos - 1] + 1)
     {
       answer1.push_back(left[xPos - 1]);
       answer2.push_back(right[yPos - 1]);
@@ -142,7 +172,7 @@ void alignOverlapTSD(std::string left, std::string right, std::ofstream& out, si
       continue;
     }
 
-    if(left[xPos - 1] != right[yPos - 1] && matrix[xPos][yPos] == matrix[xPos - 1][yPos - 1] + 1)
+    if(left[xPos - 1] != right[yPos - 1] && matrix[xPos][yPos] == matrix[xPos - 1][yPos - 1] - 3)
     {
       answer1.push_back(left[xPos - 1]);
       answer2.push_back(right[yPos - 1]);
@@ -153,42 +183,63 @@ void alignOverlapTSD(std::string left, std::string right, std::ofstream& out, si
       continue;
     }
 
-    if(matrix[xPos][yPos] == matrix[xPos - 1][yPos] + 1)
+    if(matrix[xPos][yPos] == matrix[xPos - 1][yPos] - 3)
     {
       answer1.push_back(left[xPos - 1]);
- //     answer2.push_back('-');
+      answer2.push_back('-');
       xPos--;
       numberOfLetterOfA++;
       continue;
     }
 
-    if(matrix[xPos][yPos] == matrix[xPos][yPos - 1] + 1)
+    if(matrix[xPos][yPos] == matrix[xPos][yPos - 1] - 3)
     {
- //     answer1.push_back('-');
+      answer1.push_back('-');
       answer2.push_back(right[yPos - 1]);
       yPos--;
       numberOfLetterOfB++;
       continue;
     }
   }
-  if(std::min(numberOfLetterOfA, numberOfLetterOfB) < 5)
-    return;
-  if(std::min(numberOfLetterOfA, numberOfLetterOfB) <= 6 && distance >= 2)
-    return;
   std::reverse(answer1.begin(), answer1.end());
   std::reverse(answer2.begin(), answer2.end());
+
+  int distance = 0;
+  for(int i = 0; i < answer1.length(); ++i)
+  {
+    if(answer1[i] != answer2[i])
+    {
+      distance++;
+    }
+  }
+
+  if((!(distance >= 3)) && std::min(numberOfLetterOfA, numberOfLetterOfB) >= 5)
+  {
+    if(!(std::min(numberOfLetterOfA, numberOfLetterOfB) <= 6 && distance >= 2))
+    {
+      myRepeat.isTSD = true;
+      myRepeat.leftTSD = answer1;
+      myRepeat.rightTSD = answer2;
+      myRepeat.editDistance = distance;
+      myRepeat.lenghtOfTSD = answer1.length();
+      myRepeat.startPosLeftTSD = myRepeat.start - left.length() + xPos;
+      myRepeat.startPosRightTSD = myRepeat.end + yPos;
+    }
+  }
+
+  myRepeat.Print(out);
   if(mapOfAnnotation.find(startOfRepeat + 1) != mapOfAnnotation.end())
   {
-    out << answer1 << " " << answer2 << " " << mapOfAnnotation[startOfRepeat + 1].family << std::endl;
-    typesAndTSD[mapOfAnnotation[startOfRepeat + 1].family].push_back(answer1);
+    typesAndTSD[mapOfAnnotation[startOfRepeat + 1].family].push_back(answer1 + " " + answer2);
   }
+
 }
 
 
 void findTSD(std::string filename)
 {
   std::ifstream in(filename);
-  std::ofstream out(filename + ".out");
+  std::ofstream out(filename + ".newout");
 
   std::string nameOfChrom;
   in >> nameOfChrom;
@@ -200,15 +251,33 @@ void findTSD(std::string filename)
     size_t startOfRegion = it.second.start - 1;
     size_t endOfRegion = it.second.end;
     size_t length = endOfRegion - startOfRegion;
-    if((double)expectedLengthOfRepeat[it.second.family]/(double)length > 1.1 || (double)expectedLengthOfRepeat[it.second.family]/(double)length < 0.9)
+
+    if((it.second.family == "L1HS" && length <= 6000) || it.second.name != "LINE/L1")
+    {
       continue;
+    }
+
+    if(it.second.family != "L1HS" && ((expectedLengthOfRepeat[it.second.family] == 0 && length > 1000)  || (double)expectedLengthOfRepeat[it.second.family]/(double)length < 0.95))
+    {
+ //     std::cout << length << " "<< expectedLengthOfRepeat[it.second.family] << " " << it.second.family << std::endl;
+      continue;
+    }
+
     //As we know the usual TSD length is 6-20 bp. So we will find such a strings
-    int leftBorderOfLeftTSD = std::max(0, (int)std::max(genome.find_last_of("N", startOfRegion - 1) + 1, startOfRegion - 20));
+    int leftBorderOfLeftTSD = std::max(0, (int)startOfRegion - 300);
     std::string leftTSD = genome.substr(leftBorderOfLeftTSD, startOfRegion - leftBorderOfLeftTSD);
-    int rightBorderOfLeftTSD = std::min(genome.length() - 1, std::min(genome.find_first_of("N", endOfRegion), endOfRegion + 20));
+    int rightBorderOfLeftTSD = std::min(genome.length() - 1, endOfRegion + 300);
     std::string rightTSD = genome.substr(endOfRegion, rightBorderOfLeftTSD - endOfRegion);
+
+    std::transform(leftTSD.begin(), leftTSD.end(), leftTSD.begin(), ::toupper);
+    std::transform(rightTSD.begin(), rightTSD.end(), rightTSD.begin(), ::toupper);    
     if(leftTSD != "")
-      alignOverlapTSD(leftTSD, rightTSD, out, startOfRegion);
+    {
+      if(it.second.family == "L1HS" )
+        std::cout << "Hi! Trying to align myself - " << leftTSD << " and " << rightTSD << std::endl;
+
+      alignOverlapTSD(leftTSD, rightTSD, out, startOfRegion, it.second);
+    }
   }
   std::ofstream secondout(filename + ".second.out");
   for(auto it : typesAndTSD)
